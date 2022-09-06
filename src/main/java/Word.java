522d43dc374ca92c49792ca72738dfbc9426a023
LedF1oyd <defieroji@gmail.com>
2022-09-07 00:14:38 +0900

    update all

--- src/main/java/Word.java
+++ src/main/java/Word.java
@@ -1,4 +1,6 @@
 public class Word {
+
+
     private int id;
     private int level;
     private String word;
@@ -43,6 +45,13 @@ public class Word {
     public void setMeaning(String meaning) {
         this.meaning = meaning;
     }
-
+    @Override
+    public String toString() {
+        String slevel ="";
+        for(int i = 0; i <level ; i++) slevel+="*";
+        String str = String.format("%-3s", slevel)
+                +String.format("%15s", word) +" "+ meaning;
+        return str;
+    }
 
 }
--- src/main/java/WordCRUD.java
+++ src/main/java/WordCRUD.java
@@ -1,7 +1,27 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class WordCRUD implements ICRUD{ //ICRUD를 구현
+    ArrayList<Word> list;
+    Scanner s;
+    WordCRUD(Scanner s){
+        list = new ArrayList<>();
+        this.s = s;
+    }
     @Override
     public Object add() {
-        return null;
+        System.out.print("=> 난이도(1,2,3) & 새로운 영단어 입력: ");
+        int level = s.nextInt();
+        String word = s.nextLine();
+        System.out.println("뜻 입력: ");
+        String meaning = s.nextLine();
+        return new Word(0, level, word, meaning);
+    }
+    public void addWord(){
+        Word one = (Word)add();
+        list.add(one);
+        System.out.println("새 단어가 단어장에 추가되었습니다.");
+
     }
 
     @Override
@@ -17,5 +37,11 @@ public class WordCRUD implements ICRUD{ //ICRUD를 구현
     @Override
     public void selectOne(int id) {
 
+    }
+    public void listAll(){
+        System.out.println("-----------------------------------");
+        for(int i=0;i<list.size();i++){
+            System.out.print((i+1)+" ");
+        }
     }
 }
