235bca22fba4f2777b847e55ba091a66f9b33f05
LedF1oyd <defieroji@gmail.com>
2022-09-13 01:40:12 +0900

    add deletItem

--- src/main/java/WordCRUD.java
+++ src/main/java/WordCRUD.java
@@ -87,5 +87,22 @@ public class WordCRUD implements ICRUD{ //ICRUD를 구현
     }
 
     public void deleteItem() {
+        System.out.println("=> 삭제할 영단어 검색: ");
+        String keyword = s.next();
+        ArrayList<Integer> idlist = this.listAll(keyword);
+        System.out.print("=> 삭제할 번호 선택: ");
+        int id = s.nextInt();
+        s.nextLine();
+
+        System.out.print("=>정말로 삭제하시겠습니까? (Y/n) ");
+        String ans = s.next();
+        if(ans.equalsIgnoreCase("y")){
+            list.remove((int)idlist.get(id-1));
+            System.out.println("단어가 삭제되었습니다.");
+        }
+        else{
+            System.out.println("취소되었습니다.");
+        }
+
     }
 }
