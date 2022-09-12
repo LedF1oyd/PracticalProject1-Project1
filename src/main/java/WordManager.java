b356b96f5ae92a1aebd5344300b9ba237fd1bf14
LedF1oyd <defieroji@gmail.com>
2022-09-12 22:57:41 +0900

    add updateitem

--- src/main/java/WordManager.java
+++ src/main/java/WordManager.java
@@ -34,6 +34,13 @@ public class WordManager {
             else if(menu==1){
                 wordCRUD.listAll();
             }
+            else if(menu==5){
+                //update
+                wordCRUD. updateItem();
+            }
+            else if(menu==6){
+                //delete
+            }
 
 
 
