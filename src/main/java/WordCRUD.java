5e6c2853af8d970a02d7fa67ce5c590895cf37b7
LedF1oyd <defieroji@gmail.com>
2022-09-17 16:54:35 +0900

    delete never used

--- src/main/java/WordCRUD.java
+++ src/main/java/WordCRUD.java
@@ -25,21 +25,6 @@ public class WordCRUD implements ICRUD{ //ICRUD를 구현
         System.out.println("새 단어가 단어장에 추가되었습니다.");
 
     }
-
-    @Override
-    public int update(Object obj) {
-        return 0;
-    }
-
-    @Override
-    public int delete(Object obj) {
-        return 0;
-    }
-
-    @Override
-    public void selectOne(int id) {
-
-    }
     public void listAll(){
         if(list.size()==0){
             System.out.println("아직 등록된 단어가 없습니다.\n단어를 등록하여 주세요.\n  ");
